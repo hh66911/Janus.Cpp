52fa7a97964e74a85b2cd41e322d0991cf31421d
xinhang huang <h1318658200@outlook.com>
2025-02-05 04:47:57 +0800

    pre-cpu,llama-cuda

--- Janus.Cpp/Janus.Cpp.cpp
+++ Janus.Cpp/Janus.Cpp.cpp
@@ -15,6 +15,7 @@
 #include <cstdlib>
 #include <memory>
 #include <vector>
+#include <array>
 #include <string>
 #include <ranges>
 #include <filesystem>
@@ -83,19 +84,30 @@ class LlamaDecoderLayer
 private:
 	std::vector<uint8_t> graph_buffer;
 	ggml_context* layer_ctx = nullptr;
-	ggml_backend* layer_backend = nullptr;
+	ggml_backend* has_backend;
 public:
-	LlamaDecoderLayer(ggml_type type, ggml_backend* container, bool quant = false)
+	LlamaDecoderLayer(
+		ggml_backend* backend = nullptr
+	) : has_backend(backend)
 	{
-		layer_backend = container;
-		ggml_init_params layer_param = {
-			.mem_size = ggml_tensor_overhead() * 7,
-			.mem_buffer = nullptr,
-			.no_alloc = true
-		};
-		layer_ctx = ggml_init(layer_param);
-		ggml_type original = type;
-		if (quant) type = GGML_TYPE_Q8_K;
+		ggml_type type = GGML_TYPE_Q8_K;
+		if (has_backend)
+		{
+			ggml_init_params layer_param = {
+				.mem_size = ggml_tensor_overhead() * 7,
+				.mem_buffer = nullptr,
+				.no_alloc = true
+			};
+			layer_ctx = ggml_init(layer_param);
+		}
+		else
+		{
+			ggml_init_params layer_param = {
+				.mem_size = ggml_tensor_overhead() * 7 +
+					(4096ull * 4096 * 4 + 4096ull * 11008 * 3) * ggml_type_size(type), // ~400 MB
+			};
+			layer_ctx = ggml_init(layer_param);
+		}
 		q_proj = ggml_new_tensor_2d(layer_ctx, type, 4096u, 4096u);
 		k_proj = ggml_new_tensor_2d(layer_ctx, type, 4096u, 4096u);
 		v_proj = ggml_new_tensor_2d(layer_ctx, type, 4096u, 4096u);
@@ -110,7 +122,8 @@ public:
 		ggml_set_name(gate_proj, "gate_proj");
 		ggml_set_name(up_proj, "up_proj");
 		ggml_set_name(down_proj, "down_proj");
-		ggml_backend_alloc_ctx_tensors(layer_ctx, container);
+		if (has_backend)
+			ggml_backend_alloc_ctx_tensors(layer_ctx, backend);
 		if (quant)
 		{
 			std::vector<float> original(ggml_nelements(q_proj));
@@ -123,6 +136,17 @@ public:
 
 	~LlamaDecoderLayer() {
 		ggml_free(layer_ctx);
+	}
+
+	void FillTo(LlamaDecoderLayer& layer)
+	{
+		ggml_backend_tensor_set(layer.q_proj, q_proj->data, 0, ggml_nbytes(q_proj));
+		ggml_backend_tensor_set(layer.k_proj, k_proj->data, 0, ggml_nbytes(k_proj));
+		ggml_backend_tensor_set(layer.v_proj, v_proj->data, 0, ggml_nbytes(v_proj));
+		ggml_backend_tensor_set(layer.o_proj, o_proj->data, 0, ggml_nbytes(o_proj));
+		ggml_backend_tensor_set(layer.gate_proj, gate_proj->data, 0, ggml_nbytes(gate_proj));
+		ggml_backend_tensor_set(layer.up_proj, up_proj->data, 0, ggml_nbytes(up_proj));
+		ggml_backend_tensor_set(layer.down_proj, down_proj->data, 0, ggml_nbytes(down_proj));
 	}
 
 	ggml_cgraph* build_llama_layer(
@@ -146,8 +170,7 @@ public:
 		ggml_set_name(input_emb, "input_emb");
 
 		auto pos_ids = ggml_new_tensor_1d(ctx, GGML_TYPE_I32, input_len);
-		auto pos_ids_data = std::views::iota(0, int32_t(input_len));
-		std::copy(pos_ids_data.begin(), pos_ids_data.end(), static_cast<int32_t*>(pos_ids->data));
+		ggml_set_name(pos_ids, "pos_ids");
 
 		ggml_tensor* output = nullptr;
 		{
@@ -169,17 +192,47 @@ public:
 			q = ggml_rope_inplace(ctx, q, pos_ids, LlamaDecoderLayer::head_dim, 0);
 			k = ggml_rope_inplace(ctx, k, pos_ids, LlamaDecoderLayer::head_dim, 0);
 			v = ggml_rope_inplace(ctx, v, pos_ids, LlamaDecoderLayer::head_dim, 0);
-			q = ggml_cast(ctx, q, GGML_TYPE_BF16);
-			k = ggml_cast(ctx, k, GGML_TYPE_BF16);
-			v = ggml_cast(ctx, v, GGML_TYPE_BF16);
+			if (!has_backend)
+			{
+				q = ggml_cast(ctx, q, GGML_TYPE_BF16);
+				k = ggml_cast(ctx, k, GGML_TYPE_BF16);
+				v = ggml_cast(ctx, v, GGML_TYPE_BF16);
+			}
 
-			// Shape: [batch, num_head, seq_len, head_dim]
-			auto attn_output = ggml_flash_attn_ext(ctx, q, k, v, nullptr, 1.0f, 0.0f, 0.0f);
+			ggml_tensor* attn_output = nullptr;
+			if (has_backend)
+			{
+				// Shape: [batch, num_head * head_dim, 1, seq_len]
+				k = ggml_reshape_4d(ctx, ggml_cont(ctx, ggml_permute(ctx, k, 2, 0, 1, 3)),
+					1, input_len, LlamaDecoderLayer::head_dim * LlamaDecoderLayer::num_heads, batch_size);
+				q = ggml_reshape_4d(ctx, ggml_cont(ctx, ggml_permute(ctx, q, 2, 0, 1, 3)),
+					1, input_len, LlamaDecoderLayer::head_dim * LlamaDecoderLayer::num_heads, batch_size);
+				v = ggml_reshape_4d(ctx, ggml_cont(ctx, ggml_permute(ctx, v, 2, 0, 1, 3)),
+					input_len, 1, LlamaDecoderLayer::head_dim * LlamaDecoderLayer::num_heads, batch_size);
+				// K * Q Shape: [batch, num_head * head_dim, seq_len, seq_len]
+				struct ggml_tensor* KQ = ggml_mul_mat(ctx, k, q);
+				// KQ_scaled = KQ / sqrt(n_embd/n_head)
+				struct ggml_tensor* KQ_scaled = ggml_scale_inplace(ctx, KQ,
+					1.0f / sqrt(float(LlamaDecoderLayer::head_dim) / LlamaDecoderLayer::num_heads));
+				// KQ_masked = mask_past(KQ_scaled)
+				// struct ggml_tensor* KQ_masked = ggml_diag_mask_inf_inplace(ctx, KQ_scaled, n_past);
+				// KQ = soft_max(KQ_masked)
+				struct ggml_tensor* KQ_soft_max = ggml_soft_max_inplace(ctx, KQ);
+				// KQV = transpose(V) * KQ_soft_max
+				attn_output = ggml_permute(ctx, ggml_mul_mat(ctx, KQ_soft_max, v), 2, 0, 3, 1);
+				attn_output = ggml_reshape_3d(ctx, ggml_cont(ctx, attn_output),
+					LlamaDecoderLayer::head_dim * LlamaDecoderLayer::num_heads, input_len, batch_size);
+			}
+			else
+			{
+				// Shape: [batch, num_head, seq_len, head_dim]
+				auto attn_output = ggml_flash_attn_ext(ctx, q, k, v, nullptr, 1.0f, 0.0f, 0.0f);
+				// 调整注意力输出形状到 [batch, seq_len, num_head * head_dim]
+				attn_output = ggml_permute(ctx, attn_output, 0, 2, 1, 3);
+				attn_output = ggml_reshape_3d(ctx, ggml_cont(ctx, attn_output),
+					LlamaDecoderLayer::head_dim * LlamaDecoderLayer::num_heads, input_len, batch_size);
+			}
 
-			// 调整注意力输出形状到 [batch, seq_len, num_head * head_dim]
-			attn_output = ggml_permute(ctx, attn_output, 0, 2, 1, 3);
-			attn_output = ggml_reshape_3d(ctx, ggml_cont(ctx, attn_output),
-				LlamaDecoderLayer::head_dim * LlamaDecoderLayer::num_heads, input_len, batch_size);
 			attn_output = ggml_mul_mat(ctx, o_proj, attn_output);
 
 			// 层归一化
@@ -204,22 +257,36 @@ public:
 
 	std::vector<uint8_t> run_layer(
 		std::vector<uint8_t>& input_embs_data,
+		ggml_gallocr* layer_galloc,
 		size_t batch_size,
 		size_t input_len,
-		ggml_gallocr* gallocr,
 		ggml_type type = GGML_TYPE_BF16
 	)
 	{
+		if (!has_backend)
+		{
+			std::cerr << "Not implemented without backend" << std::endl;
+			throw std::runtime_error("Not implemented without backend");
+		}
+
 		auto gr = build_llama_layer(batch_size, input_len);
-		ggml_gallocr_reserve(gallocr, gr);
-		auto mem_size = ggml_gallocr_get_buffer_size(gallocr, 0);
-		std::cout << "Layer mem usage: " << std::setprecision(2)
+		ggml_gallocr_reserve(layer_galloc, gr);
+		auto mem_size = ggml_gallocr_get_buffer_size(layer_galloc, 0);
+		std::cout << "Layer mem usage: " << std::fixed << std::setprecision(2)
 			<< mem_size / 1024. / 1024. << "MB" << std::endl;
-		if (!ggml_gallocr_alloc_graph(gallocr, gr))
+		if (!ggml_gallocr_alloc_graph(layer_galloc, gr))
 			throw std::runtime_error("Cannot allocate graph in LlamaDecoderLayer");
+
 		auto input_embs_tensor = ggml_graph_get_tensor(gr, "input_emb");
 		ggml_backend_tensor_set(input_embs_tensor, input_embs_data.data(), 0, input_embs_data.size());
-		ggml_backend_graph_compute(layer_backend, gr);
+
+		auto pos_ids = ggml_graph_get_tensor(gr, "pos_ids");
+		auto pos_ids_generator = std::views::iota(0, int32_t(input_len));
+		std::vector<int> pos_ids_data(input_len);
+		std::copy(pos_ids_generator.begin(), pos_ids_generator.end(), pos_ids_data.begin());
+		ggml_backend_tensor_set(pos_ids, pos_ids_data.data(), 0, ggml_nbytes(pos_ids));
+
+		ggml_backend_graph_compute(has_backend, gr);
 		auto output = ggml_graph_node(gr, -1);
 		std::vector<uint8_t> result(ggml_nbytes(output));
 		ggml_backend_tensor_get(output, result.data(), 0, result.size());
@@ -247,9 +314,9 @@ class LanguageModel
 {
 private:
 	static constexpr int pad_id = 100015;
+	ggml_context* model_ctx = nullptr;
 	ggml_backend* cuda_backend = nullptr;
 	ggml_backend* blas_backend = nullptr;
-	ggml_context* model_ctx = nullptr;
 	const size_t gpu_offload_num;
 public:
 	LanguageModel(
@@ -272,10 +339,15 @@ public:
 		ggml_backend_tensor_set(input_embeddings, 0, 0, 0); // TODO: 从文件加载
 
 		layers.reserve(30);
-		for (auto i : std::views::iota(0ull, gpu_offload_layer_num))
-			layers.emplace_back(type, cuda_backend);
-		for (auto i : std::views::iota(gpu_offload_layer_num, 30ull))
-			layers.emplace_back(type, blas_backend);
+		for (auto i : std::views::iota(0ull, 30ull))
+			layers.emplace_back(type);
+
+		offloads.reserve(gpu_offload_num);
+		for (auto i : std::views::iota(0ull, gpu_offload_num))
+		{
+			offloads.emplace_back(type, cuda_backend);
+			layers[i].FillTo(offloads[i]);
+		}
 	}
 	~LanguageModel() {
 		ggml_free(model_ctx);
@@ -284,40 +356,37 @@ public:
 	}
 
 	std::vector<uint8_t> preprocess(
-		std::vector<int> input_ids_data,
+		std::vector<int> input_ids,
 		size_t parallel_size = 16,
 		size_t image_token_num_per_image = 576
 	)
 	{
-		const size_t reserved_size =
-			ggml_tensor_overhead() * GGML_DEFAULT_GRAPH_SIZE + ggml_graph_overhead();
-		std::vector<uint8_t> graph_buffer(reserved_size);
-		ggml_init_params builder_params = {
-			.mem_size = graph_buffer.size(),
-			.mem_buffer = graph_buffer.data(),
-			.no_alloc = true
-		};
-		auto ctx = ggml_init(builder_params);
-
-		auto pre_graph = ggml_new_graph(ctx);
-		auto input_len = input_ids_data.size();
-		auto input_ids = ggml_new_tensor_1d(ctx, GGML_TYPE_I32, input_len);
+		auto input_len = input_ids.size();
 
 		// Shape: [parallel_size * 2, input_len]
-		auto tokens = ggml_new_tensor_2d(ctx, GGML_TYPE_I32, input_len, parallel_size * 2ull);
-		tokens = ggml_repeat(ctx, input_ids, tokens);
-		ggml_tensor* pad_id = nullptr;
-		pad_id = ggml_new_tensor_1d(ctx, GGML_TYPE_I32, 1);
-		auto tokens_to_pad = ggml_view_2d(
-			ctx, tokens, parallel_size, input_len - 1,
-			input_len * ggml_type_size(GGML_TYPE_I32),
-			(input_len + 1) * ggml_type_size(GGML_TYPE_I32)
-		);
-		auto repeated_pad_id = ggml_repeat(ctx, pad_id, tokens_to_pad);
-		tokens_to_pad = ggml_set_2d_inplace(ctx,
-			tokens_to_pad, repeated_pad_id,
-			tokens_to_pad->nb[1], 0);
+		std::vector<int> tokens_data(input_len * parallel_size * 2ull);
+#pragma omp parallel for
+		for (int i = 0; i < int(parallel_size) * 2; i++)
+		{
+			if (i % 2 == 0)
+				std::copy(input_ids.begin(), input_ids.end(), tokens_data.begin() + i * input_len);
+			else
+			{
+				tokens_data[i * input_len] = input_ids[0];
+				std::fill(tokens_data.begin() + i * input_len + 1,
+					tokens_data.begin() + (i + 1) * input_len, pad_id);
+			}
+		}
 
+		ggml_init_params builder_params = {
+			.mem_size = (tokens_data.size() * 4097) * 4 +
+				ggml_tensor_overhead() * GGML_DEFAULT_GRAPH_SIZE + ggml_graph_overhead(),
+		};
+		auto ctx = ggml_init(builder_params);
+		auto pre_graph = ggml_new_graph(ctx);
+
+		auto tokens = ggml_new_tensor_2d(ctx, GGML_TYPE_I32, input_len, parallel_size * 2ull);
+		std::copy(tokens_data.begin(), tokens_data.end(), static_cast<int*>(tokens->data));
 		// 获取输入嵌入
 		ggml_tensor* inputs_embeds = ggml_get_rows(ctx,
 			language_model->input_embeddings,
@@ -326,16 +395,10 @@ public:
 		inputs_embeds = ggml_reshape_3d(ctx, inputs_embeds, 4096, input_len, parallel_size * 2ull);
 
 		ggml_build_forward_expand(pre_graph, inputs_embeds);
-		auto gallocr = ggml_gallocr_new(ggml_backend_get_default_buffer_type(blas_backend));
-		ggml_gallocr_reserve(gallocr, pre_graph);
-		auto mem_size = ggml_gallocr_get_buffer_size(gallocr, 0);
-		std::cout << "Preprocess graph memory size: " << mem_size << std::endl;
-		ggml_gallocr_alloc_graph(gallocr, pre_graph);
-		ggml_backend_graph_compute(blas_backend, pre_graph);
+		ggml_graph_compute_with_ctx(ctx, pre_graph, 16);
 		
 		std::vector<uint8_t> result(ggml_nbytes(inputs_embeds));
-		ggml_backend_tensor_get(inputs_embeds, result.data(), 0, result.size());
-		ggml_gallocr_free(gallocr);
+		memcpy(result.data(), inputs_embeds->data, result.size());
 		ggml_free(ctx);
 		return result;
 	}
@@ -347,12 +410,12 @@ public:
 	)
 	{
 		auto cuda_ga = ggml_gallocr_new(ggml_backend_get_default_buffer_type(cuda_backend));
-		auto blas_ga = ggml_gallocr_new(ggml_backend_get_default_buffer_type(blas_backend));
 
 		for (auto i : std::views::iota(0ull, gpu_offload_num))
 		{
-			auto& layer = layers[i];
-			input_embs_data = layer.run_layer(input_embs_data, parallel_size * 2, input_len, cuda_ga);
+			auto& layer = offloads[i];
+			input_embs_data = layer.run_layer(
+				input_embs_data, cuda_ga, parallel_size * 2, input_len);
 		}
 
 		return input_embs_data;
@@ -360,6 +423,7 @@ public:
 private:
 	ggml_tensor* input_embeddings = nullptr;
 	std::vector<LlamaDecoderLayer> layers;
+	std::vector<LlamaDecoderLayer> offloads;
 	struct GenHead
 	{
 		ggml_context* gen_head_ctx = nullptr;
@@ -507,7 +571,7 @@ void test()
 int main(int argc, char** argv)
 {
 	{
-		language_model = new LanguageModel(GGML_TYPE_BF16);
+		language_model = new LanguageModel(18, GGML_TYPE_BF16);
 		std::vector<int> input(16);
 		auto embeddings = language_model->preprocess(input);
 		language_model->run_model(embeddings, 16, 16);
